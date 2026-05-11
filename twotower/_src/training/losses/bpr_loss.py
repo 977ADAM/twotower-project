10d32bd04cc75e2e4433183fa349d5e90c50c111
977ADAM <977team.95@gmail.com>
2026-05-11 11:54:36 +0300

    perf: hoist LogSigmoid to module level in compute_bpr_loss

--- twotower/_src/training/losses/bpr_loss.py
+++ twotower/_src/training/losses/bpr_loss.py
@@ -4,8 +4,9 @@ import torch.nn as nn
 
 from twotower._src.training.losses._types import LossInputs, LossResult
 
+_LOG_SIGMOID = nn.LogSigmoid()
+
 
 def compute_bpr_loss(inputs: LossInputs) -> LossResult:
-    criterion = nn.LogSigmoid()
-    loss = -criterion(inputs.positive_scores - inputs.negative_scores).mean()
+    loss = -_LOG_SIGMOID(inputs.positive_scores - inputs.negative_scores).mean()
     return LossResult(loss=loss)
